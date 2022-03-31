375dd0ca71eb8b35611e72628e0392b35088bf1d
Robert Taylor <rvtaylor94@gmail.com>
2022-03-31 15:18:21 -0600

    created basic express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
 //main server file for "npm run server-dev"
+const express = require('express');
+let app = express();
+
+app.use(express.static(__dirname + '/../public'));
+app.use(express.json());
+
+
+
+const port = 3000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(`Listening on port ${port}`);
+})
