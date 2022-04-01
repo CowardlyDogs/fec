0c0f6cb8a5810130468ae6b3ebc7f42b9503902b
Austin Chapin <austinchapin04@gmail.com>
2022-04-01 16:02:28 -0700

    Corrected helper functions - attached directly to module.exports

--- src/components/Questions/HelperFunction.js
+++ src/components/Questions/HelperFunction.js
@@ -1,29 +1,29 @@
 // Data helper functions
 
-// Sorting questions from product based on **helpfulness**
-var sortQuestions = (resData) => {
-  var results = resData.results.sort( (a, b) => {
-    return  b.question_helpfulness - a.question_helpfulness
-  })
+module.exports={
+
+
+  // Sorting questions from product based on **helpfulness**
+  sortQuestions: (resData) => {
+    var results = resData.results.sort( (a, b) => {
+     return  b.question_helpfulness - a.question_helpfulness
+    })
   return results;
+  },
+  //  Output = High->Low   [ {}, {}, {}, {} ]
+
+
+
+  // Sorting answers from single question
+  sortAnswers: (question) => {
+    var results = [];
+    for (const key in question.answers) {
+      results.push(question.answers[key])
+    };
+    results.sort( (a, b) => {
+        return  b.helpfulness - a.helpfulness
+      });
+    return results;
+  }
+  //  Output = High->Low   [ {}, {}, {}, {} ]
 }
-//  Output = High->Low   [ {}, {}, {}, {} ]
-
-
-
-// Sorting answers from single question
-var sortAnswers = (question) => {
-  var results = [];
-  for (const key in question.answers) {
-    results.push(question.answers[key])
-   };
-   results.sort( (a, b) => {
-      return  b.helpfulness - a.helpfulness
-    });
-   return results;
-}
-//  Output = High->Low   [ {}, {}, {}, {} ]
-
-
-// exports.module.sortAnswers = sortAnswers;
-// exports.module.sortQuestions = sortQuestions;
