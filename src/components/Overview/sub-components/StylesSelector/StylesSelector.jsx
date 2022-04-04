a1b201078946b31a72ef093cb7b80b5f5dbd5b3c
Robert Taylor <rvtaylor94@gmail.com>
2022-04-04 11:54:31 -0600

    divided styles into divs of 4

--- src/components/Overview/sub-components/StylesSelector/StylesSelector.jsx
+++ src/components/Overview/sub-components/StylesSelector/StylesSelector.jsx
@@ -6,21 +6,42 @@ function StylesSelector() {
   const product = useContext(OverviewContext);
   console.log(product);
 
+  var productRows = [];
+  var groupedStyles = [];
+  var numStyles = 0;
+  for (let i = 0; i < product.results.length; i++) {
+    if (groupedStyles.length === 4) {
+      productRows.push(groupedStyles);
+      groupedStyles = [];
+      numStyles = 0;
+    }
 
+    groupedStyles.push(product.results[i]);
+    numStyles += 1;
+
+  }
+  if (groupedStyles.length > 0) {
+    productRows.push(groupedStyles);
+  }
 
   return (
-    <div>
+    <div className="styles-selector">
       {/* TODO: Delete Styles Selector Title */}
       <h1>Styles Selector</h1>
       {
-        product.results.map((style) => {
-          return (
-            <div>
-              <span className="style-title">{style.name}</span>
-              <img className="style-thumbnail" src={style.photos[0].thumbnail_url} width={50} height={50} />
-            </div>
-          )
-        })
+        productRows.map((row) =>
+          <div className="styles-row">
+            {
+              row.map((style) =>
+                <div className="style">
+                  <span className="style-title">{style.name}</span>
+                  <img className="style-thumbnail" src={style.photos[0].thumbnail_url} width={50} height={50} />
+                </div>
+              )
+            }
+          </div>
+        )
+
       }
     </div>
   );
