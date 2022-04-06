2dfba24c0f2574bff2c79a6aa7bf6cc072946908
Andrew <andrew.orodenker@gmail.com>
2022-04-05 18:50:31 -0700

    temp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Outfit from './Related/Outfit.jsx';
 import Carousel from './Related/Carousel.jsx';
 import items from './Related/sampledata.jsx';
+import RelatedItems from './Related/RelatedItems.jsx';
 
 const App = () => {
   return (
   <div className="App">
     <Carousel items={items}/>
     <Outfit/>
+    <RelatedItems/>
   </div>
   )
 }
