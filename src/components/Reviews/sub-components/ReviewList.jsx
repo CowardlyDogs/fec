4d7adc56fd22fceb8eb5e6753d4faaaea4c4470e
Tristan L <tristan97little@gmail.com>
2022-04-08 13:13:13 -0600

    Eslint corrections. Eslinter now working properly

--- src/components/Reviews/sub-components/ReviewList.jsx
+++ src/components/Reviews/sub-components/ReviewList.jsx
@@ -2,17 +2,17 @@ import React/*, { useState }*/ from 'react';
 import Review from './Review.jsx';
 
 
-function ReviewList({page, turnPage, product, reviews, helpful, report}) {
+var ReviewList = ({page, turnPage, product, reviews, helpful, report}) => {
 
   var nextPage = () => {
     turnPage('inc');
-  }
+  };
   var prevPage = () => {
     turnPage('dec');
-  }
+  };
 
   return (
-    // <h1>Ratings and Reviews Will Go Here</h1>
+  // <h1>Ratings and Reviews Will Go Here</h1>
     /*
     ***TODO***
     *
@@ -25,18 +25,18 @@ function ReviewList({page, turnPage, product, reviews, helpful, report}) {
       <div>
         <ul>
 
-            {reviews.map(review => (
-              <Review review={review} key={review.review_id} helpful={helpful} report={report}/>
-        ))}
+          {reviews.map(review => (
+            <Review review={review} key={review.review_id} helpful={helpful} report={report}/>
+          ))}
         </ul>
       </div>
       <div className="review-nav">
-        {(page > 1) ? (<div><button onClick={prevPage}>Previous Page</button></div>) : ""}
+        {(page > 1) ? (<div><button onClick={prevPage}>Previous Page</button></div>) : ''}
         {(page === 0) ? (<div><button onClick={nextPage}>Show More</button></div>) : (<div><button onClick={nextPage}>Next Page</button></div>)}
 
       </div>
-   </div>
+    </div>
   );
-}
+};
 
 export default ReviewList;
--- src/components/Reviews/sub-components/StarRating.jsx
+++ src/components/Reviews/sub-components/StarRating.jsx
@@ -4,17 +4,17 @@ import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 
 
 
-function Rating({rating}) {
+var Rating = ({rating}) => {
 
-    return (
-      <div className="Star-Ratings">
-        {(rating >= 1) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
-        {(rating >= 2) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
-        {(rating >= 3) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
-        {(rating >= 4) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
-        {(rating >= 5) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
-      </div>
-    )
-}
+  return (
+    <div className="Star-Ratings">
+      {(rating >= 1) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
+      {(rating >= 2) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
+      {(rating >= 3) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
+      {(rating >= 4) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
+      {(rating >= 5) ? <StarIcon className='StarIcon'/> : <StarBorderOutlinedIcon className='StarIconEmpty'/> }
+    </div>
+  );
+};
 
 export default Rating;
