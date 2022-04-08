6ffb4314b1616ee4c92cd717f75f9fdba305651f
Robert Taylor <rvtaylor94@gmail.com>
2022-04-08 12:03:30 -0600

    moved axios calls to one place

--- src/components/APIHelpers.js
+++ src/components/APIHelpers.js
@@ -1,7 +1,27 @@
 import axios from 'axios';
 
-module.exports = {
-  getData()
-  //axios.get
-  //do stuff
-}
+const APIHelpers = {
+  // getData(),
+
+  getStyles: (productId, callback) => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/${productId}/styles`)
+      .then((results) => {
+        callback(null, results.data);
+      })
+      .catch((err) => {
+        callback(err);
+      })
+  },
+
+  getProductInfo: (productId, callback) => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/${productId}`)
+    .then((results) => {
+      callback(null, results.data);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }
+};
+
+export default APIHelpers;
--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -5,44 +5,49 @@ import StylesSelector from './sub-components/StylesSelector/StylesSelector.jsx';
 import Cart from './sub-components/Cart/Cart.jsx';
 import ImageGallery from './sub-components/ImageGallery/ImageGallery.jsx';
 import authorization from '../../../config.js';
-const axios = require('axios');
+import APIHelpers from '../APIHelpers.js';
 
 export const OverviewContext = React.createContext(null);
 
 function Overview () {
 
-  const [styles, setStyles] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [currentStyle, setCurrentStyle] = useState({});
+  const [styles       , setStyles       ] = useState({});
+  const [currentStyle , setCurrentStyle ] = useState({});
+  const [productInfo  , setProductInfo  ] = useState({});
+  const [loading      , setLoading      ] = useState(true);
 
 
 
   useEffect(() => {
 
-    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40344/styles', {
-      headers: {
-        Authorization: authorization.TOKEN
+    APIHelpers.getStyles(65631, (err, res) => {
+      if (err) {
+        console.error(err);
+      } else {
+        setStyles(res);
+        setCurrentStyle(res.results[0]);
+        setLoading(false);
       }
     })
-      .then((results) => {
-        setStyles(results.data);
-        setCurrentStyle(results.data.results[0]);
-        setLoading(false);
-      })
-      .catch((err) => {
+
+    APIHelpers.getProductInfo(65631, (err, res) => {
+      if (err) {
         console.error(err);
-      })
+      } else {
+        setProductInfo(res);
+      }
+    })
 
   }, []);
 
   if (loading) {
-    return (<div></div>)
+    return (null)
   }
 
 
   return (
     <OverviewContext.Provider value={{currentStyle: currentStyle, styles: styles}}>
-      <ProductInfo />
+      <ProductInfo productInfo={productInfo}/>
       <StylesSelector setCurrentStyle={setCurrentStyle} />
       <Cart />
       <ImageGallery />
